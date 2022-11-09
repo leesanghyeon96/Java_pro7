a253b7553cd9c66cb32ecee27ab93d843b865f52
Sanghyeon Lee <lgs4630@naver.com>
2022-11-09 12:51:06 +0900

    2022년 11월9일

---
+++ src/Chapter01/Outprint.java
@@ -0,0 +1,12 @@
+package Chapter01;
+
+public class Outprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//
+		System.out.println("오늘의 %s는 매우 맑습니다.");
+	}
+
+}
