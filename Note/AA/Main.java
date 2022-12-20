de468bd155a7575998624c213df1b38139109e35
Sanghyeon Lee <lgs4630@naver.com>
2022-12-20 18:26:56 +0900

    12-20 백준커밋

--- Note/AA/Main.java
+++ Note/AA/Main.java
@@ -6,17 +6,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
+		
 		int sum = 0;
 		
-		for(int i = 1 ; i <= a ; i++) {
+		
 			int one = sc.nextInt();
 			int two = sc.nextInt();
 			sum = one + two;
-			System.out.println("Case #" + i + ": " + sum);
-		}
+			
+			
+			System.out.println(sum);
+		
 	
-		sc.close();
+		
 	}
 
 }
