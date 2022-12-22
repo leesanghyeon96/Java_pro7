bde95e78fadf62623ac6a63dc27ccae7935a4b14
Sanghyeon Lee <lgs4630@naver.com>
2022-12-22 22:40:47 +0900

    백준문제

--- Note/AA/Main.java
+++ Note/AA/Main.java
@@ -6,17 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int sum = 0;
+		int t = sc.nextInt();
+		int o = sc.nextInt();
 		
-		for(int i = 1 ; i <= a ; i++) {
-			int one = sc.nextInt();
-			int two = sc.nextInt();
-			sum = one + two;
-			System.out.println("Case #" + i + ": " + sum);
-		}
-	
-		sc.close();
+		t=(o%10)*10;	//십의 자리수
+		
+		
+		
+		
+		
+		
+		
+	sc.close();
 	}
 
 }
---
+++ one/BackJoon/Loop_For_While__/Eight.java
@@ -0,0 +1,26 @@
+package BackJoon.Loop_For_While__;
+
+import java.util.Scanner;
+
+public class Eight {
+
+	public static void main(String[] args) {
+		// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+		// 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i = 1; i<=a;i++) {
+			for(int j = 1 ; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
